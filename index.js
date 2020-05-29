1e88f6d4ccf913a3df6f60fd9752706ffd225179
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-05-29 07:47:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
-  .then(respon =>respon.json());
+  .then(respon =>respon.json())
+  .then(json=>console.log(json));
 }
 
 function renderBooks(json) {
